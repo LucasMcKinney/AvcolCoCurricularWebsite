6a2d4dcd37832cb40b0dd54460ad2f76ceacb254
Lucas McKinney <ac111562@avcol.school.nz>
2022-08-31 18:35:52 +1200

    Add data context and Activities page using Activity data model.

---
+++ AvcolCoCurricularWebsite/Data/AvcolCoCurricularWebsiteContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using AvcolCoCurricularWebsite.Models;
+
+namespace AvcolCoCurricularWebsite.Data
+{
+    public class AvcolCoCurricularWebsiteContext : DbContext
+    {
+        public AvcolCoCurricularWebsiteContext (DbContextOptions<AvcolCoCurricularWebsiteContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<AvcolCoCurricularWebsite.Models.Activity> Activity { get; set; }
+    }
+}
---
+++ AvcolCoCurricularWebsite/Pages/Activities/Index.cshtml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using AvcolCoCurricularWebsite.Data;
+using AvcolCoCurricularWebsite.Models;
+
+namespace AvcolCoCurricularWebsite.Pages.Activities
+{
+    public class IndexModel : PageModel
+    {
+        private readonly AvcolCoCurricularWebsite.Data.AvcolCoCurricularWebsiteContext _context;
+
+        public IndexModel(AvcolCoCurricularWebsite.Data.AvcolCoCurricularWebsiteContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Activity> Activity { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Activity = await _context.Activity
+                .Include(a => a.Staff).ToListAsync();
+        }
+    }
+}
