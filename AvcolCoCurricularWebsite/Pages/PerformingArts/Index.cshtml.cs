ecb9c6707c8144d6a8c0194c3b72f54683935dfd
Lucas McKinney <ac111562@avcol.school.nz>
2022-09-16 14:22:57 +1200

    Added paging to Performing Arts Index page.

--- AvcolCoCurricularWebsite/Pages/PerformingArts/Index.cshtml.cs
+++ AvcolCoCurricularWebsite/Pages/PerformingArts/Index.cshtml.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using AvcolCoCurricularWebsite.Data;
 using AvcolCoCurricularWebsite.Models;
 
@@ -13,22 +14,35 @@ namespace AvcolCoCurricularWebsite.Pages.PerformingArts
     public class IndexModel : PageModel
     {
         private readonly AvcolCoCurricularWebsite.Data.AvcolCoCurricularWebsiteContext _context;
+        private readonly IConfiguration Configuration;
 
-        public IndexModel(AvcolCoCurricularWebsite.Data.AvcolCoCurricularWebsiteContext context)
+        public IndexModel(AvcolCoCurricularWebsite.Data.AvcolCoCurricularWebsiteContext context, IConfiguration configuration)
         {
             _context = context;
+            Configuration = configuration;
         }
 
         public string ActivitySort { get; set; }
         public string CurrentFilter { get; set; }
         public string CurrentSort { get; set; }
 
-        public IList<PerformingArt> PerformingArt { get;set; }
+        public PaginatedList<PerformingArt> PerformingArts { get; set; }
 
-        public async Task OnGetAsync(string sortOrder, string searchString)
+        public async Task OnGetAsync(string sortOrder, string currentFilter, string searchString, int? pageIndex)
         {
             // using System;
+            CurrentSort = sortOrder;
             ActivitySort = sortOrder == "Activity" ? "activity_desc" : "Activity";
+
+            if (searchString != null)
+            {
+                pageIndex = 1;
+            }
+            else
+            {
+                searchString = currentFilter;
+            }
+
             CurrentFilter = searchString;
 
             IQueryable<PerformingArt> performingartsIQ = from p in _context.PerformingArt
@@ -49,7 +63,8 @@ namespace AvcolCoCurricularWebsite.Pages.PerformingArts
                     break;
             }
 
-            PerformingArt = await performingartsIQ.Include(p => p.Activity).AsNoTracking().ToListAsync();
+            var pageSize = Configuration.GetValue("PageSize", 10);
+            PerformingArts = await PaginatedList<PerformingArt>.CreateAsync(performingartsIQ.Include(p => p.Activity).AsNoTracking(), pageIndex ?? 1, pageSize);
         }
     }
 }
