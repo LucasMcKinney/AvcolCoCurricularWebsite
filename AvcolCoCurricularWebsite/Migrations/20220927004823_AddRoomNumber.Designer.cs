e818c2e463539ccf6c782c8401e50258b193c4fb
Lucas McKinney <ac111562@avcol.school.nz>
2022-09-27 14:08:57 +1300

    Added Room Number migration by removing Room Number references from all group models and pages, then using Package Manager Console to to add the migration and update the database.

--- AvcolCoCurricularWebsite/Migrations/20220927004823_AddRoomNumber.Designer.cs
+++ AvcolCoCurricularWebsite/Migrations/20220927004823_AddRoomNumber.Designer.cs
@@ -4,14 +4,16 @@ using AvcolCoCurricularWebsite.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace AvcolCoCurricularWebsite.Migrations
 {
     [DbContext(typeof(AvcolCoCurricularWebsiteContext))]
-    partial class AvcolCoCurricularWebsiteContextModelSnapshot : ModelSnapshot
+    [Migration("20220927004823_AddRoomNumber")]
+    partial class AddRoomNumber
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ AvcolCoCurricularWebsite/Migrations/20220927004823_AddRoomNumber.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AvcolCoCurricularWebsite.Migrations
+{
+    public partial class AddRoomNumber : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RoomNumber",
+                table: "SubjectTutorial");
+
+            migrationBuilder.DropColumn(
+                name: "RoomNumber",
+                table: "Sport");
+
+            migrationBuilder.DropColumn(
+                name: "RoomNumber",
+                table: "ScholarshipTutorial");
+
+            migrationBuilder.DropColumn(
+                name: "RoomNumber",
+                table: "PerformingArt");
+
+            migrationBuilder.DropColumn(
+                name: "RoomNumber",
+                table: "Music");
+
+            migrationBuilder.DropColumn(
+                name: "RoomNumber",
+                table: "Club");
+
+            migrationBuilder.AddColumn<string>(
+                name: "RoomNumber",
+                table: "Activity",
+                maxLength: 3,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RoomNumber",
+                table: "Activity");
+
+            migrationBuilder.AddColumn<string>(
+                name: "RoomNumber",
+                table: "SubjectTutorial",
+                type: "nvarchar(3)",
+                maxLength: 3,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "RoomNumber",
+                table: "Sport",
+                type: "nvarchar(3)",
+                maxLength: 3,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "RoomNumber",
+                table: "ScholarshipTutorial",
+                type: "nvarchar(3)",
+                maxLength: 3,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "RoomNumber",
+                table: "PerformingArt",
+                type: "nvarchar(3)",
+                maxLength: 3,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "RoomNumber",
+                table: "Music",
+                type: "nvarchar(3)",
+                maxLength: 3,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "RoomNumber",
+                table: "Club",
+                type: "nvarchar(3)",
+                maxLength: 3,
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
