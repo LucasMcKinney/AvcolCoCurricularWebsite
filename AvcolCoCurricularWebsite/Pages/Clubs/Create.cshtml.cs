9ab659715dd25880a0e8cc055b53aec9a516299c
Lucas McKinney <ac111562@avcol.school.nz>
2022-10-28 10:11:30 +1300

    Development on Room occupation validation in Clubs create page.

--- AvcolCoCurricularWebsite/Pages/Clubs/Create.cshtml.cs
+++ AvcolCoCurricularWebsite/Pages/Clubs/Create.cshtml.cs
@@ -18,6 +18,7 @@ public class CreateModel : PageModel
 
     [BindProperty]
     public Club Club { get; set; }
+    //public Activity Activity { get; set; }
     public string StartTimeErrorMessage { get; set; }
     public string EndTimeErrorMessage { get; set; }
     public string RoomErrorMessage { get; set; }
@@ -44,9 +45,25 @@ public class CreateModel : PageModel
             return Page();
         }
 
-        Club roomOccupied = (from c in _context.Club
+        /*
+        Club roomOccupied2 = (from c in _context.Club
                              where c.Activity.RoomNumber == Club.Activity.RoomNumber && c.Day == Club.Day // && c.StartTime >= Club.StartTime && c.EndTime <= Club.EndTime
                              select c).FirstOrDefault(); // checks if the room set for this activity is occupied between the same start and end times of the day specified on the activity
+        */
+
+        var activity = await _context.Activity.FindAsync(Club.ActivityID);
+
+        if (activity == null)
+        {
+            // an error occured, please try again
+            ViewData["ActivityID"] = new SelectList(_context.Activity, "ActivityID", "ActivityName");
+            StartTimeErrorMessage = "Invalid Start Time. Start Time cannot be greater or equal to End Time."; // displays error message
+            return Page();
+        }
+
+        Club.Activity = activity;
+
+        Club roomOccupied = _context.Club.Where(c => c.Activity.RoomNumber == Club.Activity.RoomNumber && c.Day == Club.Day).FirstOrDefault();
 
         if (roomOccupied != null)
         {
@@ -55,6 +72,7 @@ public class CreateModel : PageModel
             return Page();
         }
 
+        /*
         Club staffOccupied = (from c in _context.Club
                               where c.Activity.StaffID == Club.Activity.StaffID && c.Day == Club.Day // && c.StartTime >= Club.StartTime && c.EndTime <= Club.EndTime
                               select c).FirstOrDefault(); // checks if the room set for this activity is occupied between the same start and end times of the day specified on the activity
@@ -65,6 +83,7 @@ public class CreateModel : PageModel
             StaffErrorMessage = "The Staff In Charge of this activity is occupied at this time. Please change the Day, Start Time, or End Time."; // displays error message
             return Page();
         }
+        */
 
         _context.Club.Add(Club);
         await _context.SaveChangesAsync();
