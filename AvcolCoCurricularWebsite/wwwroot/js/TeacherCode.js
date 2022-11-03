6f99455509da880e0199098e96076bba1d87bdf4
Lucas McKinney <ac111562@avcol.school.nz>
2022-11-03 19:18:27 +1300

    Update TeacherCode JavaScript file.

--- AvcolCoCurricularWebsite/wwwroot/js/TeacherCode.js
+++ AvcolCoCurricularWebsite/wwwroot/js/TeacherCode.js
@@ -5,7 +5,7 @@
 
     if (firstName !== "" && lastName !== "") // when the entered staff name is not null (for validation)
     {
-        var teacherCode = $("#Staff_TeacherCode").val(firstName.substring(0, 1).toUpperCase() + lastName.substring(0, 2).toUpperCase());
+        $("#Staff_TeacherCode").val(firstName.substring(0, 1).toUpperCase() + lastName.substring(0, 2).toUpperCase());
         // sets the value of the teacher's code in a variable by taking the first character of the staff's first name and makes it uppercase, then adds the first 2 characters of the staff's last name and makes them uppercase
     }
 }
